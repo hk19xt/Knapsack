b6df7b465b1f1a016c919f9b341b0f8802d8c057
Hyejin Kim <81273823+hk19xt@users.noreply.github.com>
2021-08-12 13:54:41 -0400

    Add files via upload

---
+++ Assign_4_Hyejin_Kim/List.java
@@ -0,0 +1,21 @@
+package Assign_4_Hyejin_Kim;
+
+/** This class is List.
+  *
+  * @author Hyejin Kim
+  * @Student #6823116
+  * @version 1.0 (3/23/2020)**/
+
+class List {
+  
+    String name;
+    int   head;
+    List  tail;
+    
+    List (String n, int h, List t ) {
+        name = n;  
+        head = h;
+        tail = t;
+    };
+    
+}  //List
